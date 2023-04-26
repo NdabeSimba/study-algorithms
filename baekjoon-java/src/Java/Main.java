c6b563729cc988538638208c5c7a5c09742a2b6e
Juhun Park <riann3207@gmail.com>
2023-04-27 00:07:29 +0900

    commit
    April 27, 2023

---
+++ baekjoon-java/src/Java/Main.java
@@ -0,0 +1,26 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int time = sc.nextInt();
+		int min = sc.nextInt();
+
+		if (min < 45) {
+			time = (time - 1);
+			min = (min + 15);
+			if (time < 0) {
+				time = 23;
+				System.out.println(time + " " + min);
+			}
+			else {
+				System.out.println(time + " "  + min);
+			}
+		}
+		else {
+			System.out.println(time + " " + (min - 45));
+		}
+	}
+}
---
+++ baekjoon-python/2884.py
@@ -0,0 +1,28 @@
+# t, m = [int(x) for x in input().split()]
+# if t > 0:
+#     if m >= 45:
+#         m = m-45
+#         print(t, m)
+#     elif m < 45:
+#         t = t-1
+#         m = m+60
+#         print(t, m-45)
+# elif t == 0:
+#     if m >= 45:
+#         m = m-45
+#         print(t, m)
+#     elif m < 45:
+#         m = m+60
+#         t = 23
+#         print(t, m-45)
+
+# better code, efficient time-wise
+H, M = map(int, input().split())
+if M < 45 :
+    if H == 0 :
+        H = 23
+        M += 60
+    else :
+        H -= 1	
+        M += 60
+print(H, M-45)	
