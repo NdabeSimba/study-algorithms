f936c50f2810152546087e248ef8b852ecd4a7b5
Juhun Park <riann3207@gmail.com>
2023-05-03 21:59:37 +0900

    May 3, 2023

---
+++ baekjoon-java/src/Java/bronze/Main24264.java
@@ -0,0 +1,17 @@
+package Java.bronze;
+
+import java.util.Scanner;
+
+public class Main24264 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        long num = sc.nextLong();
+        // 1 ≤ num ≤ 500,000
+
+        System.out.println(num*num);
+        System.out.println(2);
+        // double loop means time complexity of O(n^2), leading coefficient of polynomial 2.
+
+    }
+}
