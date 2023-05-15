bafa03d1e2273e6f9650a3f889070e06ba2b1aa8
박주훈 <riann3207@gmail.com>
2023-05-15 12:08:31 +0900

    May 15, 2023

--- python/baekjoon/data-structures/2605.py
+++ python/baekjoon/data-structures/2605.py
@@ -1,1 +1,11 @@
-num_stu = int(input)
+num_stu = int(input())
+order = list(map(int, input().split()))
+line = []
+
+for i in range(num_stu):
+    if order[i] == 0:
+        line.append(i+1)
+    else:
+        line.insert(i-order[i],i+1)
+        
+print(*line)
