ff2105a332a1b816887c2cff96c9f4f81416e2f7
Juhun Park <riann3207@gmail.com>
2023-05-17 19:48:02 +0900

    [Bronze V] Title: A+B, Time: 236 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+	public static void main(String args[]){
+		Scanner scn = new Scanner(System.in);
+		int a, b;
+		a = scn.nextInt();
+		b = scn.nextInt();
+		System.out.println(a + b);
+	}
+}
