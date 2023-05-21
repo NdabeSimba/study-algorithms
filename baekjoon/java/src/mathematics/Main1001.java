975edd9858ea12500a5db465918c095823a2eb6b
Juhun Park <riann3207@gmail.com>
2023-05-21 17:08:08 +0900

    java add

---
+++ baekjoon/java/src/mathematics/Main1001.java
@@ -0,0 +1,14 @@
+package mathematics;
+
+import java.util.Scanner;
+
+public class Main1001 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a - b);
+        sc.close();
+    }
+}
---
+++ baekjoon/java/src/mathematics/Main1008.java
@@ -0,0 +1,17 @@
+package mathematics;
+
+import java.util.Scanner;
+
+public class Main1008 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        sc.close();
+        System.out.print(a / b);
+
+    }
+}
