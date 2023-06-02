c2fd208d2a81f9f36aeedd160c8f8d94ed495bab
Juhun Park <riann3207@gmail.com>
2023-06-02 21:41:57 +0900

    약수

---
+++ baekjoon/python/mathematics/1037-약수/main.py
@@ -0,0 +1,11 @@
+N = int(input())
+num = sorted(list(map(int, input().split())))
+
+if N == 1:
+    print(num[0] * num[0])
+else:
+    print(num[0] * num[-1])
+
+N = int(input())
+lis = list(map(int, input().split()))
+print(max(lis) * min(lis))
